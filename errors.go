d2b26f1a006477cd8b37ea8c0cb2b5def2a6cdf2
crush <wswxzy@gmail.com>
2025-09-21 21:39:12 +0800

    feat(db): 实现数据库核心 API 和启动时索引重建

--- errors.go
+++ errors.go
@@ -3,8 +3,11 @@ package bitcask_kv_go
 import "errors"
 
 var (
-	ErrKeyIsEmpty        = errors.New("the key is empty")
-	ErrIndexUpdateFailed = errors.New("failed to update index")
-	ErrKeyNotFound       = errors.New("key not found in database")
-	ErrDataFileNotFound  = errors.New("data file is not found")
+	ErrKeyIsEmpty             = errors.New("the key is empty")
+	ErrIndexUpdateFailed      = errors.New("failed to update index")
+	ErrKeyNotFound            = errors.New("key not found in database")
+	ErrDataFileNotFound       = errors.New("data file is not found")
+	ErrDataDirectoryCorrupted = errors.New("the database directory maybe corrupted")
+	ErrDatabaseDirPathIsEmpty = errors.New("database dir path is empty")
+	ErrDataFileSizeInvalid    = errors.New("database data file size must be greater than 0")
 )
--- options.go
+++ options.go
@@ -1,5 +1,15 @@
 package bitcask_kv_go
 
+type IndexerType = int8
+
+const (
+	// BTree 索引
+	BTree IndexerType = iota + 1
+
+	// ART Adaptive Radix Tree 自适应基数树索引
+	ART
+)
+
 type Options struct {
 	// 数据库数据目录
 	DirPath string
@@ -9,4 +19,7 @@ type Options struct {
 
 	// 每次写数据是否持久化
 	SyncWrites bool
+
+	// 索引类型
+	IndexType IndexerType
 }
